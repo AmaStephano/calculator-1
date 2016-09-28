504ee604fb3521f062063d823f6f29d2446d6cb8
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 12:21:08 -0700

    Working Multiplication Function

--- arithmetic.py
+++ arithmetic.py
@@ -7,7 +7,8 @@ def subtract(num1, num2):
     return num1 - num2
 
 def multiply(num1, num2):
-    pass
+    """returns the product of 2 numbers"""
+    return num1 * num2
 
 def divide(num1, num2):
     pass
